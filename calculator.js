2f2748276b9b0b7da99b85f94f951f4ecce88bfe
AmitLevi213 <amitandta@gmail.com>
2023-09-28 00:58:22 +0300

    First Commit

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const display = document.getElementById('display');
+
+function appendNumber(number) {
+  display.value = display.value + number;
+}
+
+function appendOperator(operator) {
+  display.value = display.value + operator;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculateResult() {
+  try {
+      display.value = eval(display.value);
+  } catch(e) {
+      display.value = 'Error';
+  }
+}
